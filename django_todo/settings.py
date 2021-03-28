abf137b821d0bc7daa47a39798650231f765ccbe
phil-griffith <philgriffith@outlook.com>
2021-03-28 18:55:29 +0000

    Create Heroku app and connect Postgres add in

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://rwdsfnydgbwoen:6742df7103f5cf5b5e6dbc349c8529082e5dfd9de6dd30201bcfd153315ab0b3@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/d8f46285pk243t')
 }
 
 
